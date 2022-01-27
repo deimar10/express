b2b5c2cb9b940c9828788455bda1b18ea7db55dd
deimar <deimar.parn@khk.ee>
2022-01-27 13:29:59 +0000

    “template loop use”

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require('express')
-const path = require("path");
 const app = express();
 
-// add public directory
-app.use(express.static('public'));
+const path = require('path')
+
 // add views directory path
 app.set('views', path.join(__dirname, 'views'));
 // add views templage engine
 app.set('view engine', 'ejs')
 
-app.get('/user/:username',  (req, res)=> {
-    // get parameter data from addressrow
-    let user = req.params.username;
-    // use this data in template
-    res.render('index.ejs', {username : user});
-});
+app.get('/questions',  (req, res)=> {
+    let questions = [
+        {title: "What is Node.js?", user : "Kadi", votes : "10"},
+        {title: "What is Express.js?", user : "Mike", votes : "8"}
+    ]
+    res.render('index', {questions:questions})
+})
 
 app.listen(3000, ()=> {
     console.log('Server started on http://localhost:3000/');
