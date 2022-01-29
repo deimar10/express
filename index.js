12cd78e7f48b00d55974888ebef5aa78a9a54d91
deimar <deimar.parn@khk.ee>
2022-01-29 14:49:00 +0000

    “Post request recognizing and body-parser install”

--- index.js
+++ index.js
@@ -8,13 +8,17 @@ app.set('views', path.join(__dirname, 'views'));
 // add views templage engine
 app.set('view engine', 'ejs')
 
-app.get('/questions',  (req, res)=> {
-    let questions = [
+app.get('/login',  (req, res)=> {
+  /*  let questions = [
         {title: "What is Node.js?", user : "Kadi", votes : "10"},
         {title: "What is Express.js?", user : "Mike", votes : "8"}
-    ]
-    res.render('index', {questions:questions})
+    ] */
+    res.render('login')
 })
+
+app.post('/login', (req, res)=>{
+    console.log('post request done')
+});
 
 app.listen(3000, ()=> {
     console.log('Server started on http://localhost:3000/');
